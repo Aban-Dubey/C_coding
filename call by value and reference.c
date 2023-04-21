53232cf38cf1e129ea43377706406c094e944cfb
Aban_Dubey <131015588+Aban-Dubey@users.noreply.github.com>
2023-04-21 21:21:55 +0530

    Add files via upload

---
+++ call by value and reference.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+void call_by_value(int m, int n) //the copy of actual parameters is used to perform operations
+{
+	m = m+n;
+	printf("The values of m and n are = %d %d \n",m,n);
+}
+
+void call_by_ref(int *a, int *b) //operations are performed on the values at address of the actual parameters
+{
+	*a = *a+*b;
+	printf("The values of a and b are = \n");
+	printf("%d %d \n",*a,*b);
+}
+
+void main()
+{
+	int x,y;
+	printf("Enter the value of x = \n");
+	scanf("%d",&x);
+	printf("Enter the value of y = \n");
+	scanf("%d",&y);
+	call_by_value(x,y); //original values of x and y are not altered
+	printf("The values of x and y after calling call by value are = %d %d \n",x,y);
+	call_by_ref(&x,&y); //original values of x and y are altered
+	printf("The values of x and y after calling call by reference are = %d %d \n",x,y);
+	printf("%d %d",x,y);
+}
